6888a1e7cb606175b8de02b6ccbdafce5e547647
João KeceK <jfabrao05@gmail.com>
2026-03-21 16:03:49 -0300

    WIP BaseCard behaviour

--- Assets/Scripts/Card/BaseCard.cs
+++ Assets/Scripts/Card/BaseCard.cs
@@ -1,7 +1,9 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class BaseCard : MonoBehaviour, ICardActivatable
 {
+    [Title("Card Properties")]
     [SerializeField] private string cardName;
     [SerializeField] private string description;
     [SerializeField] private Sprite cardImage;
