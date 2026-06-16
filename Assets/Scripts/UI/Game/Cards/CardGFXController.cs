fc45e80f89f30fad8e2ccc33bcde8b93bbd70c39
João KeceK <jfabrao05@gmail.com>
2026-06-15 23:07:31 -0300

    Card Visual Cost working

--- Assets/Scripts/UI/Game/Cards/CardGFXController.cs
+++ Assets/Scripts/UI/Game/Cards/CardGFXController.cs
@@ -10,10 +10,13 @@ public class CardGFXController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI titleLabel;
     [SerializeField] private Image cardIconImage;
     [SerializeField] private Image cardIconBackground;
+    [SerializeField] private Image cardCostGfx;
+    [SerializeField] private Color costDefaultColor;
+    [SerializeField] private Color costNoManaColor;
     
     private BaseClientManaManager _clientManaManager;
     private int _cardCost;
-
+    
     public void Initialize(CardDataSO cardDataSo, BaseClientManaManager clientManaManager)
     {
         _clientManaManager  = clientManaManager;
@@ -27,9 +30,23 @@ public class CardGFXController : MonoBehaviour
         UpdateCardCostGfx();
     }
 
+    private void Update()
+    {
+        if (_clientManaManager != null)
+            UpdateCardCostGfx();
+    }
+    
     private void UpdateCardCostGfx()
     {
         float remaining =  Mathf.Clamp01((_cardCost - _clientManaManager.PredictedMana) / _cardCost);
-        GameLog.Info($"Cost: {_cardCost} - Mana: {_clientManaManager.PredictedMana} Remaining: {remaining}");
+        cardIconBackground.fillAmount = remaining;
+
+        cardCostGfx.color = costNoManaColor;
+        cardCostGfx.color = Color.Lerp(cardCostGfx.color, costDefaultColor, (1f - remaining));
+
+        /*if (remaining <= 0)
+            cardCostGfx.color = Color.Lerp(cardCostGfx.color, costDefaultColor, (1f - remaining));
+        else
+            cardCostGfx.color = Color.Lerp(cardCostGfx.color, costNoManaColor, (1f - remaining));*/
     }
 }
