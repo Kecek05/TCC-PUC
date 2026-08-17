866131b8408b0a6da09e11ccf1b7951ddec0bebb
João KeceK <jfabrao05@gmail.com>
2026-08-17 11:27:52 -0300

    Bot Deck fixed

--- Assets/Scripts/Gameplay/Cards/Tower/TowerCardSubFactory.cs
+++ Assets/Scripts/Gameplay/Cards/Tower/TowerCardSubFactory.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class TowerCardSubFactory : BaseCardSubFactory
-{did you fini
+{
     [SerializeField] private GhostTowerCard ghostTowerCard;
 
     public override bool CanHandle(CardDataSO data) => data is TowerCardDataSO;
